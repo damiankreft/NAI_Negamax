643c9f81de4120eaa92a1051bb4295b2b8f69001
s-kreft <sebastian.kreft.sg@gmail.com>
2023-10-11 21:06:33 +0200

    Add docstrings to project

--- 01_game_battleships.py
+++ 01_game_battleships.py
@@ -4,10 +4,10 @@ from easyAI import TwoPlayerGame, Negamax, Human_Player, AI_Player
 
 
 class Battleships(TwoPlayerGame):
-    """In turn, the players remove one, two or three bones from a
-    pile of bones. The player who removes the last bone loses."""
+    """Each player has one ship with 17 combat power. Goal of the game is to sink opponent's battleship before he sink our ship. 
+        In turn, the players use one of two actions: strzal (-2 cp for enemy's ship and -1 cp for ours) or 
+        salwa (-4 cp for enemy's ship and -3 cp for ours). The player who stays afloat wins"""
 
-    # chyba zrobiony
     def __init__(self, players=None):
         self.players = players
         self.b1 = 17
@@ -18,14 +18,19 @@ class Battleships(TwoPlayerGame):
         return ["strzal", "salwa"]
 
     def make_move(self, move):
-        if move == "strzal": # y 1, o 2
+        """Possible actions to perform in game. 
+        "strzal" action subtracts 2 combat power points from ememy's cp pool and 1 cp from player's cp pool
+        "salwa" action subtracts 4 combat power points from ememy's cp pool and 3 cp from player's cp pool
+        """
+        
+        if move == "strzal":
             if self.current_player is 1: # shot fired by player b1
                 self.b1 -= 1
                 self.b2 -= 2
             else: # shot fired by player b2
                 self.b2 -= 1
                 self.b1 -= 2
-        elif "salwa": # y 3, o 4
+        elif "salwa":
             if self.current_player is 1:
                 self.b1 -= 3
                 self.b2 -= 4
@@ -35,6 +40,8 @@ class Battleships(TwoPlayerGame):
 
     # zrobiony
     def win(self):
+        """If opponent ship's combat point pool is <= 0 then player wins
+        """
         if self.current_player == 1:
             return self.b2 <= 0
         elif self.current_player == 2:
@@ -42,15 +49,16 @@ class Battleships(TwoPlayerGame):
         
     # zrobiony
     def is_over(self):
+        """Geme ends when combat power of one of the ships is <= 0"""
         return self.b1 <= 0 or self.b2 <= 0  # game stops when someone wins.
 
     # zrobiony
     def scoring(self):
-        # return 100 if self.b1 <= 0 else 0
         return 100 if self.win() else 0
 
     # todo wypisywac ile hp maja statki
     def show(self):
+        """After each turn function prints actual value of combat power for both ships"""
         print("AI's battleship: %d, player's battleship: %d " % (self.b1, self.b2))
 
 ai = Negamax(10) # The AI will think 10 moves in advance
