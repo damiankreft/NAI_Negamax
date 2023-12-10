3c2bd9431419692ed655c032c78b12a88ffef119
Damian Kreft <damian.kreft@dgt.eu>
2023-12-10 18:46:06 +0100

    Remove dataset\'s readme file, rename main.py to wine.py"

---
+++ NeuralNetwork/animals_model.py
@@ -0,0 +1,58 @@
+import tensorflow as tf   
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.layers import Input, Conv2D, Dense, Flatten, Dropout, GlobalMaxPooling2D, MaxPooling2D, BatchNormalization
+from tensorflow.keras.models import Model
+
+model = tf.keras.models.load_model('animals.h5')
+# Load in the data
+cifar10 = tf.keras.datasets.cifar10
+ 
+# Distribute it to train and test set
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+# Reduce pixel values
+x_train, x_test = x_train / 255.0, x_test / 255.0
+ 
+# flatten the label values
+y_train, y_test = y_train.flatten(), y_test.flatten()
+
+# number of classes
+K = len(set(y_train))
+ 
+# calculate total number of classes 
+# for output layer
+print("number of classes:", K)
+
+batch_size = 32
+data_generator = tf.keras.preprocessing.image.ImageDataGenerator(
+  width_shift_range=0.1, height_shift_range=0.1, horizontal_flip=True)
+ 
+train_generator = data_generator.flow(x_train, y_train, batch_size)
+steps_per_epoch = x_train.shape[0] // batch_size
+print(x_train.shape)
+labels = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+ 
+# index of an image to test
+image_number = 18
+ 
+# display the image
+plt.imshow(x_test[image_number])
+ 
+# load the image in an array
+n = np.array(x_test[image_number])
+ 
+# reshape it
+p = n.reshape(1, 32, 32, 3)
+ 
+# pass in the network for prediction and 
+# save the predicted label
+prediction = model.predict(p).argmax()
+predicted_label = labels[model.predict(p).argmax()]
+ 
+# load the original label
+original_label = labels[y_test[image_number]]
+
+# display the result
+print("Original label is {} and predicted label is {}".format(
+    original_label, predicted_label))
